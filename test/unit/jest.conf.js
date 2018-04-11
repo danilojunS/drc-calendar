f0000c70571e424d4c75474d2c617986ee5bb7b4
Danilo Jun <danilojunS@users.noreply.github.com>
2018-04-11 03:20:55 -0300

    add travis config

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -19,7 +19,7 @@ module.exports = {
   mapCoverage: true,
   coverageDirectory: '<rootDir>/test/unit/coverage',
   collectCoverageFrom: [
-    'src/**/*.{js,vue}',
+    'src/**/*.{js}',
     '!src/main.js',
     '!src/router/index.js',
     '!src/**/*.story.js',
